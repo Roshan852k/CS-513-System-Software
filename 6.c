28ff6e7a832ee27d2ba61bb19bedebb69e5812c5
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2024-08-25 00:22:39 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,32 @@
+/* Name - Roshan Yadav
+   Roll No - MT2024169
+   
+   Problem - Write a program to take input from STDIN and display on STDOUT. Use only read/write system calls
+*/
+
+#include <unistd.h>
+#include <stdio.h>  
+
+void main()
+{
+    char *buffer[1];
+    int CountSourcRead, CountDestinationWrite;
+    while(1)
+    {
+        CountSourcRead = read(STDIN_FILENO, buffer, 1);
+        CountDestinationWrite = write(STDOUT_FILENO, buffer, 1);
+
+        if (CountSourcRead == -1 || CountDestinationWrite == -1)
+        {
+            printf("Error while reading or writing");
+            break;
+        }
+    }
+}
+
+/*
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ gcc 6.c -o 6
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ./6
+My name is Roshan Yadav
+My name is Roshan Yadav
+*/
