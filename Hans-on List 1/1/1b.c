aae3a95be3e1f367f6c6f3fc267fcb5606f61632
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2024-08-27 18:53:26 +0530

    Add files via upload

---
+++ Hans-on List 1/1/1b.c
@@ -0,0 +1,30 @@
+/* Name - Roshan Yadav
+   Roll No - MT2024169
+   
+   Problem - Create the following types of a files using (i) shell command (ii) system call - hard link (link system call)
+*/
+
+
+#include<unistd.h>
+#include<stdio.h>
+int main()
+{
+	int file_desc;
+	file_desc=link("test.txt","hardlink");
+	if(file_desc==-1)
+	{
+		printf("Error!");
+	}
+	else{
+	        printf("Hardlink is cretaed successfully\n");
+	}
+	return 0;
+}
+
+/*
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ gcc 1b.c -o 1b
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ./1b
+Hardlink is cretaed successfully
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ll hardlink
+-rw-rw-r-- 3 roshan roshan 30 Aug 15 20:04 hardlink
+*/
---
+++ Hans-on List 1/1/1c.c
@@ -0,0 +1,42 @@
+/* Name - Roshan Yadav
+   Roll No - MT2024169
+   
+   Problem - Create the following types of a files using (i) shell command (ii) system call - FIFO (mkfifo Library Function or mknod system call)
+*/
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include <sys/stat.h>
+
+void main(){
+    char *mkfifoName = "./mymkfifo";    
+    char *mknodName = "./mymknod-fifo"; 
+    
+    int mkfifo_stat = mkfifo(mkfifoName, S_IRWXU);
+    if(mkfifo_stat == -1){
+      printf("Error during creating pipe using mkfifo\n");
+    }
+    else{
+      printf("pipe created successfully using mkfifo\n");
+    }
+    
+    int mknod_stat = mknod(mknodName, S_IRWXU, 0);
+    if(mknod_stat == -1){
+      printf("Error during creating pipe using mknod\n");
+    }
+    else{
+      printf("pipe created successfully using mknod\n");
+    }
+}
+
+/*
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ gcc 1c.c -o 1c
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ./1c
+pipe created successfully using mkfifo
+pipe created successfully using mknod
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ll mymkfifo mymknod-fifo
+prwx------ 1 roshan roshan 0 Aug 27 18:35 mymkfifo|
+-rwx------ 1 roshan roshan 0 Aug 27 18:35 mymknod-fifo*
+*/
+
