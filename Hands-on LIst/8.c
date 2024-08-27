be45def74285c72aa43b102b06ce7fc04a016030
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2024-08-27 19:14:33 +0530

    Add files via upload

---
+++ Hands-on LIst/8.c
@@ -0,0 +1,41 @@
+/* Name - Roshan Yadav
+   Roll No - MT2024169
+   
+   Problem - Write a program to open a file in read only mode, read line by line and display each line as it is read.
+Close the file when end of file is reached.
+*/
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+void main(){
+    char *buffer[1]; 
+    int CountSourcRead;
+    char * file = "read.txt";
+    
+    int file_desc   = open(file, O_RDONLY);
+    if(file_desc==-1){
+	    printf("error while opening file\n");
+    }
+    
+    while ((CountSourcRead = read(file_desc, buffer, 1)) == 1)
+    {
+        write(STDOUT_FILENO, buffer, 1);
+    }
+    
+    close(file_desc);
+}
+
+/*
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ cat read.txt
+I am Roshan Yadav
+Doing Mtech from IIITB
+Learning LInux Commnad 
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ gcc 8.c -o 8
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ./8
+I am Roshan Yadav
+Doing Mtech from IIITB
+Learning LInux Commnad 
+*/
+	
