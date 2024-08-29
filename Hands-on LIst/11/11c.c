149f4aa89839829eb752321899d1b89ab39e2c4b
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2024-08-29 19:26:11 +0530

    Add files via upload

--- Hands-on LIst/11/11c.c
+++ Hands-on LIst/11/11c.c
@@ -1,8 +1,8 @@
 /* Name - Roshan Yadav
    Roll No - MT2024169
    
-   Problem - Write a program to open a file, duplicate the file descriptor and append the file with both the
-descriptors and check whether the file is updated properly or not. - use fcntl
+   Problem - Write a program to open a file, duplicate the file descriptor and append the file with both the descriptors and check whether the file is updated properly or not. 
+           - use fcntl
 */
 
 #include<stdio.h> 
