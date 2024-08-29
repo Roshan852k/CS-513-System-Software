1ecdb92c4f6884b19fdaefb351908375d31d1534
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2024-08-29 19:22:53 +0530

    Add files via upload

--- Hands-on LIst/2.c
+++ Hands-on LIst/2.c
@@ -2,7 +2,7 @@
    Roll No - MT2024169
    
    Problem - Write a simple program to execute in an infinite loop at the background. Go to /proc directory and 
-   identify all the process related information in the corresponding proc directory.
+             identify all the process related information in the corresponding proc directory.
 */
 
 #include<stdio.h>
