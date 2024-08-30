c31d9b3e54410be38e96e756decf422029ed3a0e
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2024-08-31 01:38:51 +0530

    Add files via upload

---
+++ Hand-on List 1/1/1.sh
@@ -0,0 +1,5 @@
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ln -s test.txt link/softlink1
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ln test.txt link/hardlink1
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ mkfifo link/fifo
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ls link
+fifo  hardlink1  softlink1
