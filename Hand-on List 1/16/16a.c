673f54990cdc13f41b3e491efdd0d4b1b1b50771
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2024-08-31 01:38:19 +0530

    Add files via upload

---
+++ Hand-on List 1/16/16a.c
@@ -0,0 +1,58 @@
+/* 
+    Name - Roshan Yadav
+    Roll No - MT2024169
+    Problem - Write a program to perform mandatory locking.
+              a. Implement write lock
+*/
+
+#include<stdio.h>
+#include<unistd.h>    
+#include<fcntl.h>     
+#include<sys/types.h> 
+#include<sys/stat.h>  
+
+void main()
+{
+    char *file_name = "my_file.txt";
+    struct flock lock, lockStatus;
+    int file_desc;
+
+    lock.l_type = F_WRLCK;    
+    lock.l_whence = SEEK_SET; 
+    lock.l_start = 0;         
+    lock.l_len = 0;           
+    lock.l_pid = getpid();
+    lockStatus = lock;
+
+    file_desc = open(file_name, O_RDWR);
+
+    fcntl(file_desc, F_GETLK, &lockStatus);
+
+    if (lockStatus.l_type == F_WRLCK){
+        printf("File already has a write lock\n");
+        getchar();
+    }
+    if (lockStatus.l_type == F_RDLCK){
+        printf("File already has a read lock\n");
+    }
+ 
+    fcntl(file_desc, F_SETLK, &lock);
+    printf("File is now locked for writing\n");
+    getchar();
+
+    close(file_desc);
+}
+
+/*
+Process 1:
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ gcc 16a.c -o 16a
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ./16a
+File is now locked for writing
+
+Process 2:
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ./16a
+File is now locked for writing
+
+*/
+
+
---
+++ Hand-on List 1/16/16b.c
@@ -0,0 +1,55 @@
+/* 
+    Name - Roshan Yadav
+    Roll No - MT2024169
+    Problem - Write a program to perform mandatory locking.
+              b. Implement read lock
+*/
+
+#include<stdio.h>
+#include<unistd.h>    
+#include<fcntl.h>     
+#include<sys/types.h> 
+#include<sys/stat.h>  
+
+void main()
+{
+    char *file_name = "my_file.txt";
+    struct flock lock, lockStatus;
+    int file_desc;
+
+    lock.l_type = F_RDLCK;    
+    lock.l_whence = SEEK_SET; 
+    lock.l_start = 0;         
+    lock.l_len = 0;           
+    lock.l_pid = getpid();
+    lockStatus = lock;
+
+    file_desc = open(file_name, O_RDONLY);
+
+    fcntl(file_desc, F_GETLK, &lockStatus);
+
+    if (lockStatus.l_type == F_WRLCK){
+        printf("File already has a write lock\n");
+        getchar();
+    }
+
+    fcntl(file_desc, F_SETLK, &lock);
+    printf("File is now locked for reading\n");
+    getchar();
+
+    close(file_desc);
+}
+
+/*
+Process 1:
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ gcc 16b.c -o 16b
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ./16b
+File is now locked for reading
+
+Process 2:
+roshan@roshan-ASUS-TUF-Gaming-F15-FX506HF-FX506HF:~/Software_System/hand-on-1$ ./16b
+File is now locked for reading
+
+*/
+
+
