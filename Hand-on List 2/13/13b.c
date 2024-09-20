d4e18741e5576101a3bf4931bf09c4b69572cf47
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2024-09-20 13:11:25 +0530

    Add files via upload

--- Hand-on List 2/13/13b.c
+++ Hand-on List 2/13/13b.c
@@ -1,6 +1,6 @@
 /*  
 ============================================================================
-Name : 13a.c
+Name : 13b.c
 Author : Roshan Yadav
 Description : Write two programs: first program is waiting to catch SIGSTOP signal, the second program will send the signal (using kill system call). 
               Find out whether the first program is able to catch the signal or not.
